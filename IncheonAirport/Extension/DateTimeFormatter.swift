570b7b175711257f7d7c7a42dbfc4b9330c51621
Jun <wnsgur4092@gmail.com>
2023-12-18 20:54:56 +1100

    CP1 - RE-Design Arrival & Departure Flight View

---
+++ IncheonAirport/Extension/DateTimeFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateTimeFormatter.swift
+//  IncheonAirport
+//
+//  Created by JunHyuk Lim on 18/12/2023.
+//
+
+import Foundation
---
+++ IncheonAirport/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  IncheonAirport
+//
+//  Created by JunHyuk Lim on 18/12/2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
